8a942c5c28cfee52254bc2771af2459990dc135c
Naol Legesse <nlege001@plattsburgh.edu>
2021-07-23 11:40:50 -0400

    Singleton buttton and textview completed

--- app/src/main/java/com/example/mysingleton/MainActivity.kt
+++ app/src/main/java/com/example/mysingleton/MainActivity.kt
@@ -3,31 +3,39 @@ package com.example.mysingleton
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.example.mysingleton.MainActivity.Greeting.hello
+
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.singleID)
+        val text = findViewById<TextView>(R.id.textViewID)
+
+        button1.setOnClickListener {
+            text.text = hello
+        }
     }
-
-
 
     // Singleton Class
     object Greeting{
         var hello = "Hello Kotlin"
-
-        fun print_val(){
-            println(hello)
         }
     }
 
 
-}
 
 
 
 
 
-}
+
+
+
+
+
