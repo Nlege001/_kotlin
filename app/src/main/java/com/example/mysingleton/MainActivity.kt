4aaa4d79b495e3174f351965f7a9435099dcca9f
Naol Legesse <nlege001@plattsburgh.edu>
2021-07-23 10:35:20 -0400

    Singleton class implemented

--- app/src/main/java/com/example/mysingleton/MainActivity.kt
+++ app/src/main/java/com/example/mysingleton/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.mysingleton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+    // Singleton Class
+    object Greeting{
+        var hello = "Hello Kotlin"
+
+        fun print_val(){
+            println(hello)
+        }
+    }
+
+
+}
+
+
+
+
+
 }
